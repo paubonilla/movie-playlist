b0fc09c936315f910783bd1eb88e311f3195e9b9
Pau Bonilla <paulo111995@gmail.com>
2020-08-20 21:42:38 -0700

    finished basic starter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './components/Header'
+import WatchList from './components/WatchList'
+import Watched from './components/Watched'
+import Add from './components/Add'
 import './App.css';
+import './lib/font-awesome/css/all.min.css'
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={WatchList} />
+        <Route path="/watched" component={Watched} />
+        <Route path="/add" component={Add} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/WatchList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WatchList = () => {
+  return (
+    <div>
+      <h1>Watchlist Page</h1>
+    </div>
+  )
+}
+
+export default WatchList
